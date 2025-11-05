c222ae08dfa67b48449bf3caffbce8252987d083
Pt Emmbp <ptemmbp@gmail.com>
2025-11-05 05:44:18 +0000

    404 This page could not be found. (_for element <SlotClone>_)

---
+++ src/app/(app)/spv/team/page.tsx
@@ -0,0 +1,45 @@
+
+"use client";
+
+import { PageHeader } from "@/components/shared/page-header";
+import { TeamPerformanceTable } from "@/components/spv/team-performance-table";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card";
+import { useCurrentUser } from "@/hooks/use-current-user";
+
+export default function TeamPage() {
+  const { user, loading } = useCurrentUser();
+
+  if (loading) {
+    return <div>Memuat...</div>;
+  }
+
+  if (!user) {
+    return <div>Harap login untuk melihat halaman ini.</div>;
+  }
+
+  return (
+    <div className="space-y-8">
+      <PageHeader
+        title="Kinerja Tim Saya"
+        description="Pantau dan kelola kinerja anggota tim penjualan Anda."
+      />
+      <Card>
+        <CardHeader>
+          <CardTitle>Tabel Kinerja</CardTitle>
+          <CardDescription>
+            Menampilkan rincian penjualan dan progres target untuk setiap anggota tim.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <TeamPerformanceTable supervisorId={user.uid} />
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
