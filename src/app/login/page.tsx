da32a9787220fa78ce999c2225a7825faccce1d6
Pt Emmbp <ptemmbp@gmail.com>
2025-11-05 05:27:01 +0000

    INI TOLONG DIHAPUS YA (_for element <SlotClone>_)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,6 @@
-import { AddUserForm } from "@/components/admin/add-user-form";
+
 import { LoginForm } from "@/components/auth/login-form";
 import AppLogo from "@/components/shared/app-logo";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
-import { Button } from "@/components/ui/button";
 
 export default function LoginPage() {
   return (
@@ -19,24 +10,6 @@ export default function LoginPage() {
           <AppLogo className="h-8 w-auto text-primary" />
         </div>
         <LoginForm />
-        <div className="mt-4 text-center text-sm">
-            <Dialog>
-                <DialogTrigger asChild>
-                    <Button variant="link" className="text-muted-foreground">
-                        Create First Admin Account
-                    </Button>
-                </DialogTrigger>
-                <DialogContent className="sm:max-w-[425px] md:max-w-4xl max-h-[90vh] overflow-y-auto">
-                    <DialogHeader>
-                    <DialogTitle>Create First Admin Account</DialogTitle>
-                    <DialogDescription>
-                        Use this form to create the initial administrator account.
-                    </DialogDescription>
-                    </DialogHeader>
-                    <AddUserForm onSuccess={() => window.location.reload()} />
-                </DialogContent>
-            </Dialog>
-        </div>
       </div>
     </div>
   );
