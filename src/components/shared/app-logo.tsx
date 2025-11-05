25664ff3d7cab9a6031ef6753beea5baba7c784a
Pt Emmbp <ptemmbp@gmail.com>
2025-11-05 09:49:30 +0000

    perkecil lagi ukurannya (_for element img_)

--- src/components/shared/app-logo.tsx
+++ src/components/shared/app-logo.tsx
@@ -12,8 +12,8 @@ const AppLogo = ({ className }: { className?: string }) => {
     >
       <Image
         src="/icon.svg"
-        width={40}
-        height={40}
+        width={60}
+        height={60}
         alt="logo"
         className="h-auto w-auto"
       />
