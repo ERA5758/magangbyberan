325af4c678f407e5451cb36042643fde0b6d1a57
Pt Emmbp <ptemmbp@gmail.com>
2025-11-05 09:37:09 +0000

    ini hapus ganti dengan icon aja (_for element span_)

--- src/components/shared/app-logo.tsx
+++ src/components/shared/app-logo.tsx
@@ -1,12 +1,16 @@
+
 import { cn } from "@/lib/utils";
-import { Building2 } from "lucide-react";
+import Image from "next/image";
 
 const AppLogo = ({ className }: { className?: string }) => {
   return (
-    <div className={cn("flex items-center gap-2 text-lg font-bold tracking-tight", className)}>
-        <div className="bg-primary text-primary-foreground p-1.5 rounded-lg">
-            <Building2 className="h-5 w-5" />
-        </div>
+    <div
+      className={cn(
+        "flex items-center gap-2 text-lg font-bold tracking-tight",
+        className
+      )}
+    >
+      <Image src="/icon.svg" width={32} height={32} alt="logo" />
       <span className="font-headline">Magang By BERAN</span>
     </div>
   );
