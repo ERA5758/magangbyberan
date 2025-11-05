f284939a00ee66c74743e3c5acfba744abbf3280
Pt Emmbp <ptemmbp@gmail.com>
2025-11-05 10:17:47 +0000

    Try fixing this error: `Console Error: Error: Firebase Admin SDK service

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,5 +1,6 @@
 
 import admin from 'firebase-admin';
+import serviceAccount from './serviceAccountKey';
 
 export function initializeAdminApp() {
   // If the app is already initialized, don't try to initialize it again.
@@ -7,19 +8,8 @@ export function initializeAdminApp() {
     return admin;
   }
   
-  let serviceAccount: admin.ServiceAccount | undefined;
-
-  if (process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON) {
-    try {
-      serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON as string);
-    } catch (e) {
-      console.error('Error parsing GOOGLE_APPLICATION_CREDENTIALS_JSON:', e);
-      throw new Error('Firebase Admin SDK service account credentials are not set or are invalid. Please check your environment variables.');
-    }
-  }
-
-  if (!serviceAccount) {
-    throw new Error('Firebase Admin SDK service account credentials are not set or are invalid. Please check your environment variables.');
+  if (!serviceAccount || !serviceAccount.project_id) {
+    throw new Error('Firebase Admin SDK service account credentials are not set or are invalid.');
   }
   
   admin.initializeApp({
